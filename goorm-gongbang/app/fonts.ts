c5b2961115fabb1faeaf987c241448bf6b33f740
hyogi <rhkdgur120@naver.com>
2026-02-03 11:17:14 +0900

    fix: 전체 폰트 추가 - pr 수정 - en -> ko - .woff2로 압축률 높은 폰트로 수정
    fixes: #13

--- goorm-gongbang/app/fonts.ts
+++ goorm-gongbang/app/fonts.ts
@@ -1,14 +1,13 @@
-// app/fonts.ts
 import localFont from "next/font/local";
 
 export const pretendard = localFont({
+  src: [
+    {
+      path: "../public/fonts/pretendard/PretendardVariable.woff2",
+      weight: "100 900",
+      style: "normal",
+    },
+  ],
   variable: "--font-pretendard",
   display: "swap",
-  src: [
-    { path: "../public/fonts/pretendard/Pretendard-Light.otf", weight: "300", style: "normal" },
-    { path: "../public/fonts/pretendard/Pretendard-Regular.otf", weight: "400", style: "normal" },
-    { path: "../public/fonts/pretendard/Pretendard-Medium.otf", weight: "500", style: "normal" },
-    { path: "../public/fonts/pretendard/Pretendard-SemiBold.otf", weight: "600", style: "normal" },
-    { path: "../public/fonts/pretendard/Pretendard-ExtraBold.otf", weight: "800", style: "normal" },
-  ],
 });
--- goorm-gongbang/app/layout.tsx
+++ goorm-gongbang/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={pretendard.variable}>
+    <html lang="ko" className={pretendard.variable}>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
