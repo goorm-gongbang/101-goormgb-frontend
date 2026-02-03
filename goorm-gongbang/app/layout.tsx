446133863399fca53ba3de71c6f5ace2227adb84
8.06_hyogi <101712060+806hyogi@users.noreply.github.com>
2026-02-03 11:28:45 +0900

    feat(font): Pretendard 전역 폰트 적용(피드백 수정) (#16)
    * feat: 전체 폰트 추가
    - 디자인팀에서 정한 전체 폰트 (font-pretendard) 를 전역적으로 적용시킵니다.
    
    fixes: #13
    
    * fix: 전체 폰트 추가 - pr 수정
    - en -> ko
    - .woff2로 압축률 높은 폰트로 수정
    
    fixes: #13

--- goorm-gongbang/app/layout.tsx
+++ goorm-gongbang/app/layout.tsx
@@ -6,6 +6,7 @@ import Providers from "./providers";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { pretendard } from "./fonts";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +29,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ko" className={pretendard.variable}>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
